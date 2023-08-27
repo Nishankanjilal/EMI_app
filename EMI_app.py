fdfd2959106bd0730adfe910be9381798717021e
Nishankanjilal <132078466+Nishankanjilal@users.noreply.github.com>
2023-08-27 10:20:39 +0400

    Update EMI_app.py

--- EMI_app.py
+++ EMI_app.py
@@ -1,12 +1,10 @@
-import streamlit as st 
-def calculate_emi(p, n, r):
-	emi = p * (r/100) * (1 + r/100)**n / ((1+r/100)**n -1)
-	st.write("EMI Calculated:", round(emi,3))
-st.title("EMI Calculator")      
-principal = st.slider("Loan Amount", 1000.0, 100000.0)
-tenure = st.slider("Tenure in years", 1, 30)
-roi = st.slider("Interest Rate (% P.A.)", 1, 15)
-n = tenure * 12
-r = roi / 12
-if st.button("Calculate"):
-	calculate_emi(principal, n, r)
+st.header('EMI calculator App') 
+def emi(p,n,r):
+  output=(p*(r/12)*((1+((r/12)/100))**(n*12)))/100*(((1+((r/12)/100))**(n*12))-1)
+  return output
+tenure=st.slider('Set Tenure of a loan in months: ',1,30) 
+principal=st.slider('Set Principal Loan Amount: ',1000,1000000) 
+rate=st.slider('Set Rate of interest: ',1,15) 
+if st.button('Calculate'): 
+  value=emi(principal,tenure,rate) 
+  st.write(f'EMI calculated is {value}')
